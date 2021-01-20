57252f9aac6d8bf681e34cea94069b3d1cb2bd7a
Luis Antonio Saavedra Palacios <djkluis@gmail.com>
2021-01-20 01:43:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "example" {
  for_each = var.resource_group
-  name     = "example-rg-ld"
-  location = "West Europe"
+  name     = each.value.name
+  location = each.value.location
 }
