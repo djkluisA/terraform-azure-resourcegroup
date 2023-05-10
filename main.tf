b5037ee7f9920034cc60b1f68d2bdd4f4ffca04c
Luis Antonio Saavedra Palacios <djkluis@gmail.com>
2023-05-10 22:25:00 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
-resource "azurerm_resource_group" "example" {
- for_each = var.resource_group
-  name     = each.value.name
-  location = each.value.location
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "test" {
+  name = "1-cf24e0ec-playground-sandbox"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = ["10.0.0.0/8"]
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "sb1" {
+  name                 = "sb1"
+  address_prefixes     = ["10.0.0.0/16"]
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  resource_group_name  = data.azurerm_resource_group.test.name
 }
