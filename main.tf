053de9ca21cee312b7aa89f319d7e177380996ed
Luis Saavedra <djkluis@gmail.com>
2023-05-18 13:58:23 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,33 +1,29 @@
 
-provider "azurerm" {
-  skip_provider_registration = true
-  features {}
-}
-
-data "azurerm_resource_group" "resource_group" {
-  name = "1-a21e6146-playground-sandbox"
-}
-
-variable "address_space" {}
-
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
   address_space       = var.address_space
-  location            = data.azurerm_resource_group.resource_group.location
-  resource_group_name = data.azurerm_resource_group.resource_group.name
+
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  tags = {
+    environment = "sandbox"
+  }
 }
 
 resource "azurerm_subnet" "sbnet1" {
   name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "nic1"
-  location            = data.azurerm_resource_group.resource_group.location
-  resource_group_name = data.azurerm_resource_group.resource_group.name
+  name                      = "nic1"
+  location                  = data.azurerm_resource_group.rg.location
+  resource_group_name       = data.azurerm_resource_group.rg.name
+  internal_dns_name_label   = "nic1"
+  enable_ip_forwarding      = false
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -37,53 +33,44 @@ resource "azurerm_network_interface" "nic1" {
   }
 }
 
-resource "azurerm_virtual_machine" "vm1" {
-  name                = "vm1"
-  location            = data.azurerm_resource_group.resource_group.location
-  resource_group_name = data.azurerm_resource_group.resource_group.name
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id,
-  ]
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                  = "vm1"
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  size                  = "Standard_B2s"
 
-  storage_image_reference {
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  storage_os_disk {
-    name              = "osdisk"
+  os_disk {
+    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
-  os_profile {
-    computer_name  = "vm1"
-    admin_username = "azureuser"
-    admin_password = "Manolita3232"
-  }
+  admin_username                = "azureuser"
+  admin_password                = "Manolita3232"
+  disable_password_authentication = false
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+  tags = {
+    environment = "sandbox"
   }
+}
 
-  hardware_profile {
-    vm_size = "Standard_B2s"
-  }
+data "azurerm_resource_group" "rg" {
+  name = "1-a21e6146-playground-sandbox"
 }
 
 provider "azurerm" {
-  alias                     = "main"
-  skip_provider_registration = true
-  features {}
-}
+  version = "~> 2.0"
 
- {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.0.0"
-    }
-  }
+  skip_provider_registration = true
+
+  features {}
 }
