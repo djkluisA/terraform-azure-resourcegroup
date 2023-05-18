6b3aae4db288188cbd7399b2b3e5f3c8ce33d4c9
Luis Saavedra <djkluis@gmail.com>
2023-05-18 14:00:53 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,4 +1,54 @@
 
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  
+  subnet {
+    name           = "sbnet1"
+    address_prefix = "10.0.0.0/16"
+  }
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_virtual_network.vnet1.subnet.id
+    private_ip_address            = "10.0.0.7"
+    private_ip_address_allocation = "Static"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                  = "vm1"
+  resource_group_name   = data.azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  size                  = "Standard_B2s"
+  admin_username        = "azureuser"
+  admin_password        = "Manolita3232"
+  disable_password_authentication = false
+ 
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+
+  os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true
@@ -8,75 +58,4 @@ data "azurerm_resource_group" "rg" {
   name = "1-a21e6146-playground-sandbox"
 }
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
-  address_space       = ["10.0.0.0/16"]
-
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  tags = {
-    environment = "sandbox"
-  }
-}
-
-resource "azurerm_subnet" "sbnet1" {
-  name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-
-resource "azurerm_network_interface" "nic1" {
-  name                      = "nic1"
-  location                  = data.azurerm_resource_group.rg.location
-  resource_group_name       = data.azurerm_resource_group.rg.name
-  internal_dns_name_label   = "nic1"
-  enable_ip_forwarding      = false
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.sbnet1.id
-    private_ip_address            = "10.0.0.7"
-    private_ip_address_allocation = "Static"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "vm1" {
-  name                  = "vm1"
-  location              = data.azurerm_resource_group.rg.location
-  resource_group_name   = data.azurerm_resource_group.rg.name
-  size                  = "Standard_B2s"
-
-  os_disk {
-    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    id        = "/subscriptions/<subscription_id>/resourceGroups/<resource_group_name>/providers/Microsoft.Compute/images/<image_name>"
-  }
-
-  os_profile {
-    computer_name  = "vm"
-    admin_username = "azureuser"
-    linux_config {
-      disable_password_authentication = false
-    }
-    admin_password = "Manolita3232"
-  }
-
-  network_interface_ids = [azurerm_network_interface.nic1.id]
-  
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  tags = {
-    environment = "sandbox"
-  }
-}
+variable "address_space" {} 
