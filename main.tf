d37f9da3ddaa2e6e241b77932ec72d98bc32caa2
Luis Saavedra <djkluis@gmail.com>
2023-05-18 12:25:28 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,35 +1,86 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      
-    }
-    tls = {
-      source = "hashicorp/tls"
-      
-    }
-    
-  }  
-}
+
 provider "azurerm" {
+  skip_provider_registration = true 
+
   features {}
-  skip_provider_registration = true
 }
 
-data "azurerm_resource_group" "test" {
-  name = "1-cf24e0ec-playground-sandbox"
+data "azurerm_resource_group" "existing" {
+  name = "1-a21e6146-playground-sandbox"
 }
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
   address_space       = ["10.0.0.0/8"]
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  depends_on = [
+    azurerm_resource_group.existing
+  ]
 }
 
-resource "azurerm_subnet" "sb1" {
-  name                 = "sb1"
-  address_prefixes     = ["10.0.0.0/16"]
+resource "azurerm_subnet" "sbnet1" {
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  resource_group_name  = data.azurerm_resource_group.test.name
+  address_prefixes     = ["10.0.0.0/16"]
+
+  depends_on = [
+    azurerm_virtual_network.vnet1
+  ]
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.sbnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.7"
+  }
+
+  depends_on = [
+    azurerm_subnet.sbnet1
+  ]
+}
+
+resource "azurerm_virtual_machine" "vm1" {
+  name                  = "vm1"
+  resource_group_name   = data.azurerm_resource_group.existing.name
+  location              = data.azurerm_resource_group.existing.location
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+
+  vm_size = "Standard_B2s"
+
+  storage_os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = azurerm_virtual_machine.vm1.name
+    admin_username = "azureuser"
+    admin_password = "Manolita3232"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  depends_on = [
+    azurerm_network_interface.nic1
+  ]
 }
