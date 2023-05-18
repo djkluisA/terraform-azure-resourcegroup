21b53978d5bf1cbeff52179f6e8e8fccdf0911b1
Luis Saavedra <djkluis@gmail.com>
2023-05-18 13:05:34 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 
 provider "azurerm" {
   skip_provider_registration = true
+
   features {}
+
 }
 
 data "azurerm_resource_group" "existing" {
@@ -10,16 +12,16 @@ data "azurerm_resource_group" "existing" {
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
+  address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "sbnet1" {
-  name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.existing.name
+  name           = "sbnet1"
+  resource_group_name = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.0.0.0/16"]
+  address_prefixes = ["10.0.0.0/16"]
 }
 
 resource "azurerm_network_interface" "nic1" {
@@ -28,36 +30,35 @@ resource "azurerm_network_interface" "nic1" {
   resource_group_name = data.azurerm_resource_group.existing.name
 
   ip_configuration {
-    name                          = "nic1-configuration"
+    name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.sbnet1.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.0.7"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "vm1"
-  resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
-  size                = "Standard_B2s"
+  name                  = "vm1"
+  resource_group_name   = data.azurerm_resource_group.existing.name
+  location              = data.azurerm_resource_group.existing.location
+  size                  = "Standard_B2s"
+  network_interface_ids = [azurerm_network_interface.nic1.id]
 
-  admin_username                 = "azureuser"
-  admin_password                 = "Manolita3232"
-  disable_password_authentication = false
-
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id,
-  ]
+  os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 
-  os_disk {
-    name              = "os_disk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  admin_username                = "azureuser"
+  admin_password                = "Manolita3232"
+  disable_password_authentication = false
 }
+
