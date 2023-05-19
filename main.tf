758c18b32d73ddd884f96f76b4923a0677668b55
Luis Saavedra <djkluis@gmail.com>
2023-05-19 13:35:17 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -12,39 +12,51 @@ variable "address_space" {}
 
 variable "address_prefixes" {}
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   address_space       = var.address_space
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "sbnet1" {
   name                 = "sbnet1"
-  address_prefixes     = var.address_prefixes
   resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = var.address_prefixes
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "nic1" {
   name                = "nic1"
-  resource_group_name = data.azurerm_resource_group.example.name
   location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.example.id
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.sbnet1.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.0.7"
+    private_ip_address            = var.private_ip_address
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                  = "vm1"
-  resource_group_name   = data.azurerm_resource_group.example.name
-  location              = data.azurerm_resource_group.example.location
-  size                  = "Standard_B2s"
-  network_interface_ids = [azurerm_network_interface.example.id]
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+
+  storage_os_disk {
+    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "azureuser"
+    admin_password = "Manolita3232"
+    disable_password_authentication = false
+  }
 
   source_image_reference {
     publisher = "Canonical"
@@ -53,13 +65,9 @@ resource "azurerm_linux_virtual_machine" "example" {
     version   = "latest"
   }
 
-  os_disk {
-    name              = "vm1osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 
-  admin_username                = "azureuser"
-  admin_password                = "Manolita3232"
-  disable_password_authentication = false
+  network_interface_ids = [azurerm_network_interface.nic1.id]
 }
