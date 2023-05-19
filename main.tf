af3946cb32d90c4a013145bd15ac63b1b2b9fe42
Luis Saavedra <djkluis@gmail.com>
2023-05-19 14:54:34 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,24 +1,59 @@
 
- {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 2.0"
-      features {}
-    }
-  }
-  required_version = ">= 0.14.0"
-}
-
 provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
 
-data "azurerm_client_config" "current" {}
-
 data "azurerm_resource_group" "sandbox" {
   name = "1-3baf3667-playground-sandbox"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kvaultmv1" {
+  name                = "kvaultmv1"
+  location            = data.azurerm_resource_group.sandbox.location
+  resource_group_name = data.azurerm_resource_group.sandbox.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules       = ["188.26.198.118"]
+  }
+
+  access_policy {
+    tenant_id        = data.azurerm_client_config.current.tenant_id
+    object_id        = data.azurerm_client_config.current.object_id
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+    ]
+  }
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_key_vault_secret" "public_key" {
+  name         = "public-clave"
+  value        = tls_private_key.key.public_key_openssh
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
+}
+
+resource "azurerm_key_vault_secret" "secret_key" {
+  name         = "secret-clave"
+  value        = tls_private_key.key.private_key_pem
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
 }
 
 resource "azurerm_virtual_network" "vnet1" {
@@ -48,40 +83,6 @@ resource "azurerm_network_interface" "nic1" {
   }
 }
 
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "azurerm_key_vault" "kvaultmv1" {
-  name                = "kvaultmv1"
-  location            = data.azurerm_resource_group.sandbox.location
-  resource_group_name = data.azurerm_resource_group.sandbox.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-
-  network_acls {
-    default_action = "Deny"
-    bypass         = "AzureServices"
-    ip_rules       = ["188.26.198.118"]
-  }
-
-  access_policy {
-    tenant_id      = data.azurerm_client_config.current.tenant_id
-    object_id      = data.azurerm_client_config.current.object_id
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Recover",
-      "Backup",
-      "Restore",
-      "Purge",
-    ]
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                  = "vm1"
   location              = data.azurerm_resource_group.sandbox.location
@@ -103,23 +104,22 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 
   admin_ssh_key {
-    username = "azureuser"
+    username  = "azureuser"
     public_key = azurerm_key_vault_secret.public_key.id
   }
-}
-
-resource "azurerm_key_vault_secret" "public_key" {
-  name         = "public-clave"
-  value        = tls_private_key.key.public_key_openssh
-  key_vault_id = azurerm_key_vault.kvaultmv1.id
-}
-
-resource "azurerm_key_vault_secret" "secret_key" {
-  name         = "secret-clave"
-  value        = tls_private_key.key.private_key_pem
-  key_vault_id = azurerm_key_vault.kvaultmv1.id
 }
 
 variable "address_space" {}
 variable "address_prefixes" {}
 variable "private_ip_address" {}
+
+ {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.0"
+      features {}
+    }
+  }
+  required_version = ">= 1.0.0"
+}
