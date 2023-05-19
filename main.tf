15dfd5efd06dcc4fa55a9ade9b11f742d9372220
Luis Saavedra <djkluis@gmail.com>
2023-05-19 13:48:09 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,79 +1,63 @@
 
 provider "azurerm" {
-  features {}
-
   skip_provider_registration = true
+  features {}
 }
 
-data "azurerm_resource_group" "test" {
+data "azurerm_resource_group" "existing" {
   name = "1-3baf3667-playground-sandbox"
 }
 
+variable "address_space" {}
+
+variable "address_prefixes" {}
+
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
   address_space       = var.address_space
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
 }
 
 resource "azurerm_subnet" "sbnet1" {
   name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
+  resource_group_name  = data.azurerm_resource_group.existing.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name 
   address_prefixes     = var.address_prefixes
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = "nic1"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
-
+  name                      = "nic1"
+  location                  = data.azurerm_resource_group.existing.location
+  resource_group_name       = data.azurerm_resource_group.existing.name
   ip_configuration {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.sbnet1.id
     private_ip_address            = var.private_ip_address
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "static"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "vm1"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
-  size                = "Standard_B2s"
-
-  admin_username                 = "azureuser"
-  admin_password                 = "Manolita3232"
+  name                  = "vm1"
+  location              = data.azurerm_resource_group.existing.location
+  resource_group_name   = data.azurerm_resource_group.existing.name
+  size                  = "Standard_B2s"
+  admin_username        = "azureuser"
+  admin_password        = "Manolita3232"
   disable_password_authentication = false
-
-  storage_os_disk {
-    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+  network_interface_ids = [azurerm_network_interface.nic1.id]
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
-  network_interface_ids = [azurerm_network_interface.nic1.id]
-}
-
-variable "address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
-}
-
-variable "address_prefixes" {
-  type    = list(string)
-  default = ["10.0.1.0/24"]
-}
-
-variable "private_ip_address" {
-  type    = string
-  default = "10.0.1.4"
+  os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 }
