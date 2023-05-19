929eacdc05deb8af519d4be36773a743355d69c2
Luis Saavedra <djkluis@gmail.com>
2023-05-19 14:53:58 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,138 +1,125 @@
 
+ {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.0"
+      features {}
+    }
+  }
+  required_version = ">= 0.14.0"
+}
+
 provider "azurerm" {
-  features {}
   skip_provider_registration = true
+  features {}
 }
 
 data "azurerm_client_config" "current" {}
 
-data "azurerm_resource_group" "test" {
+data "azurerm_resource_group" "sandbox" {
   name = "1-3baf3667-playground-sandbox"
 }
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
+  location            = data.azurerm_resource_group.sandbox.location
+  resource_group_name = data.azurerm_resource_group.sandbox.name
   address_space       = var.address_space
 }
 
 resource "azurerm_subnet" "sbnet1" {
-  name                      = "sbnet1"
-  resource_group_name       = data.azurerm_resource_group.test.name
-  virtual_network_name      = azurerm_virtual_network.vnet1.name
-  address_prefixes          = var.address_prefixes
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.sandbox.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = var.address_prefixes
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                      = "nic1"
-  location                  = data.azurerm_resource_group.test.location
-  resource_group_name       = data.azurerm_resource_group.test.name
+  name                = "nic1"
+  location            = data.azurerm_resource_group.sandbox.location
+  resource_group_name = data.azurerm_resource_group.sandbox.name
 
   ip_configuration {
-    name                          = "config1"
+    name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.sbnet1.id
-    private_ip_address            = var.private_ip_address
     private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
   }
 }
 
-resource "tls_private_key" "key1" {
-  algorithm   = "RSA"
-  rsa_bits    = 4096
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "azurerm_key_vault" "kvaultmv1" {
   name                = "kvaultmv1"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
+  location            = data.azurerm_resource_group.sandbox.location
+  resource_group_name = data.azurerm_resource_group.sandbox.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  sku_name = "standard"
+  sku_name            = "standard"
 
   network_acls {
-    bypass        = "AzureServices"
-    default_action = "Allow"
-    ip_rules      = ["188.26.198.118"]
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules       = ["188.26.198.118"]
   }
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
+    tenant_id      = data.azurerm_client_config.current.tenant_id
+    object_id      = data.azurerm_client_config.current.object_id
     secret_permissions = [
-      "get",
-      "list",
-      "set",
-      "delete",
-      "recover",
-      "backup",
-      "restore",
-      "purge",
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
     ]
   }
 }
 
-resource "azurerm_key_vault_secret" "public-clave" {
-  name         = "public-clave"
-  value        = tls_private_key.key1.public_key_openssh
-  key_vault_id = azurerm_key_vault.kvaultmv1.id
-}
-
-resource "azurerm_key_vault_secret" "secret-clave" {
-  name         = "secret-clave"
-  value        = tls_private_key.key1.private_key_pem
-  key_vault_id = azurerm_key_vault.kvaultmv1.id
-}
-
-data "azurerm_image" "ubuntu" {
-  name                = "UbuntuServer"
-  resource_group_name = data.azurerm_resource_group.test.name
-}
-
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "vm1"
-  location            = data.azurerm_resource_group.test.location
-  resource_group_name = data.azurerm_resource_group.test.name
-  size                = "Standard_B2s"
-
-  storage_os_disk {
-    name                 = "os_disk"
-    caching              = "ReadWrite"
-    create_option        = "FromImage"
-    managed_disk_type    = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "vm1"
-    admin_username = "azureuser"
-    admin_ssh_key {
-      username   = "azureuser"
-      public_key = azurerm_key_vault_secret.public-clave.value
-    }
-  }
+  name                  = "vm1"
+  location              = data.azurerm_resource_group.sandbox.location
+  resource_group_name   = data.azurerm_resource_group.sandbox.name
+  size                  = "Standard_B2s"
 
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
-  network_interface_ids = [azurerm_network_interface.nic1.id]
+  admin_username = "azureuser"
+
+  os_disk {
+    name                 = "vm1-os-disk"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = azurerm_key_vault_secret.public_key.id
+  }
+}
+
+resource "azurerm_key_vault_secret" "public_key" {
+  name         = "public-clave"
+  value        = tls_private_key.key.public_key_openssh
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
+}
+
+resource "azurerm_key_vault_secret" "secret_key" {
+  name         = "secret-clave"
+  value        = tls_private_key.key.private_key_pem
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
 }
 
 variable "address_space" {}
-
 variable "address_prefixes" {}
-
 variable "private_ip_address" {}
-
- {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "= 2.0"
-    }
-  }
-}
