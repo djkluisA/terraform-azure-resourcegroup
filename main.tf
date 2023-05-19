b8b7d66b5dc6974029cc25cdd44a5842cf0a0b62
Luis Saavedra <djkluis@gmail.com>
2023-05-19 13:45:24 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,31 +1,22 @@
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-}
-
-data "azurerm_resource_group" "rg" {
-  name = "1-3baf3667-playground-sandbox"
-}
-
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
   address_space       = var.address_space
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
 }
 
 resource "azurerm_subnet" "sbnet1" {
   name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = var.address_prefixes
 }
 
 resource "azurerm_network_interface" "nic1" {
   name                = "nic1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
 
   ip_configuration {
     name                          = "ipconfig1"
@@ -37,9 +28,13 @@ resource "azurerm_network_interface" "nic1" {
 
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                = "vm1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.test.location
+  resource_group_name = data.azurerm_resource_group.test.name
   size                = "Standard_B2s"
+
+  admin_username                 = "azureuser"
+  admin_password                 = "Manolita3232"
+  disable_password_authentication = false
 
   source_image_reference {
     publisher = "Canonical"
@@ -48,17 +43,23 @@ resource "azurerm_linux_virtual_machine" "vm1" {
     version   = "latest"
   }
 
-  os_disk {
-    name              = "osdisk"
+  storage_os_disk {
+    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
     caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-  admin_username = "azureuser"
-  admin_password = "Manolita3232"
-  disable_password_authentication = false
-
   network_interface_ids = [azurerm_network_interface.nic1.id]
+}
+
+data "azurerm_resource_group" "test" {
+  name = "1-3baf3667-playground-sandbox"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
 }
 
 variable "address_space" {}
