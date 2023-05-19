e5e7f081be3e2791223f223be04e05a567c72401
Luis Saavedra <djkluis@gmail.com>
2023-05-19 13:37:46 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,77 +1,76 @@
 
-provider "azurerm" {
-  skip_provider_registration = true
-  features {
-    virtual_machine {
-      delete_os_disk_on_deletion = true
-    }
-  }
-}
-
-data "azurerm_resource_group" "example" {
-  name = "1-3baf3667-playground-sandbox"
-}
-
-variable "address_space" {}
-
-variable "address_prefixes" {}
-
-variable "private_ip_address" {}
-
-resource "azurerm_virtual_network" "vnet1" {
+resource "azurerm_virtual_network" "virtual_network" {
   name                = "vnet1"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
   address_space       = var.address_space
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
 }
 
-resource "azurerm_subnet" "sbnet1" {
+resource "azurerm_subnet" "subnet" {
   name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
+  resource_group_name  = data.azurerm_resource_group.resource_group.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = var.address_prefixes
 }
 
-resource "azurerm_network_interface" "nic1" {
+resource "azurerm_network_interface" "nic" {
   name                = "nic1"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
 
-  ip_configuration {
+  ip_configurations {
     name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.sbnet1.id
-    private_ip_address_allocation = "Static"
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address            = var.private_ip_address
+    private_ip_address_allocation = "Static"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm1" {
+resource "azurerm_latest_virtual_machine_image" "vm_image" {
+  publisher = "canonical"
+  offer     = "UbuntuServer"
+  sku       = "18.04-LTS"
+
+  version = "latest"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm1"
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  location            = data.azurerm_resource_group.resource_group.location
   size                = "Standard_B2s"
 
-  admin_username = "azureuser"
-
-  admin_password = "Manolita3232"
-
+  admin_username              = "azureuser"
+  admin_password              = "Manolita3232"
   disable_password_authentication = false
 
-  storage_os_disk {
-    name              = "${azurerm_linux_virtual_machine.vm1.name}-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
   network_interface_ids = [
-    azurerm_network_interface.nic1.id
+    azurerm_network_interface.nic.id,
   ]
+
+  source_image_reference {
+    publisher = azurerm_latest_virtual_machine_image.vm_image.publisher
+    offer     = azurerm_latest_virtual_machine_image.vm_image.offer
+    sku       = azurerm_latest_virtual_machine_image.vm_image.sku
+    version   = azurerm_latest_virtual_machine_image.vm_image.version
+  }
+
+  os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 }
+
+data "azurerm_resource_group" "resource_group" {
+  name = "1-3baf3667-playground-sandbox"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "private_ip_address" {}
