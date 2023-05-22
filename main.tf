0bc3ea6bd5987dc1a0aa766b9b0b2b398611d565
Luis Saavedra <djkluis@gmail.com>
2023-05-22 12:08:22 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-virtual-network"
   location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
-  address_space       = []
-  address_prefixes    = []
+  address_space       = var.address_space
+  address_prefixes    = var.address_prefixes
 }
 
 # define subnet
@@ -30,7 +30,7 @@ resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = []
+  address_prefixes     = var.address_prefixes
 }
 
 # define network interface
@@ -43,6 +43,11 @@ resource "azurerm_network_interface" "example" {
     name                          = "example-ipconfig"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = ""
+    private_ip_address            = var.private_ip_address
   }
 }
+
+# define variables
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "private_ip_address" {}
