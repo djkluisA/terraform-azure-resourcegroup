fa3efe549b90e1a1901f0c1f3432c4f9c7335201
Luis Saavedra <djkluis@gmail.com>
2023-05-22 12:10:57 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
 
- {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {
-  }
+  features {}
+
   skip_provider_registration = true
 }
 
