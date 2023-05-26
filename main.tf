9610aeea48e1530072885198c6a33133c3297750
djkluisA <djkluis@gmail.com>
2023-05-26 14:53:52 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -6,13 +6,11 @@ variable "private_ip_address" {}
 
 # Configuración del proveedor de AWS
 provider "aws" {
-  region                  = "us-east-1"
+  region = "us-east-1"
+  skip_metadata_api_check = true
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
   skip_provider_registration = true
-  features {
-    cloudwatch_logs = {
-      log_group_name = "-logs"
-    }
-  }
 }
 
 # Creación de la red virtual
