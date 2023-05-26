f91ea05b64d47b28d92d7bad6e44b3bff700f9f9
djkluisA <djkluis@gmail.com>
2023-05-26 14:59:16 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ variable "private_ip_address" {}
 # Configuración del proveedor de AWS
 provider "aws" {
   region = "us-east-1"
+  features {}
   skip_metadata_api_check = true
   skip_credentials_validation = true
   skip_requesting_account_id = true
