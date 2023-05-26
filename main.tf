6a44f945e842cf51a55b7e4c31c9d619ae0b65d9
djkluisA <djkluis@gmail.com>
2023-05-26 11:46:12 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,2 +1,55 @@
 
-network_interface_ids = [azurerm_network_interface.nic1.id]
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "resource group1-dcddda38-playground-sandbox"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.rg.name}/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/subnet1"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic1" {
+  network_interface_id      = azurerm_network_interface.nic1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = "vnet1"
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+variable "address_space" {}
+
+variable "address_prefixes" {}
+
+variable "private_ip_address" {}
