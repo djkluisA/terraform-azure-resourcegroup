145645ebf4cbb8b9795187a27bdd397e33ff0973
djkluisA <djkluis@gmail.com>
2023-05-26 09:59:29 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -46,7 +46,6 @@ resource "azurerm_network_interface_backend_address_pool_association" "nic1" {
 resource "azurerm_lb_backend_address_pool" "pool1" {
   name                = "pool1"
   loadbalancer_id     = azurerm_lb.lb1.id
-  backend_address_pool_name = "pool1"
 }
 
 resource "azurerm_lb" "lb1" {
