2ddfaca211b41fab84c6db48dba61159ffb94471
djkluisA <djkluis@gmail.com>
2023-05-26 11:14:58 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -4,11 +4,25 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
+data "azurerm_client_config" "current" {}
+
 data "azurerm_resource_group" "rg" {
-  name = "1-d25caae9-playground-sandbox"
+  name = "1-dcddda38-playground-sandbox"
 }
 
-data "azurerm_client_config" "current" {}
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sbnet1" {
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = var.address_prefixes
+}
 
 resource "azurerm_network_interface" "nic1" {
   name                = "nic1"
@@ -17,55 +31,80 @@ resource "azurerm_network_interface" "nic1" {
 
   ip_configuration {
     name                          = "ipconfig1"
-    subnet_id                     = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.rg.name}/providers/Microsoft.Network/virtualNetworks/vnet1/subnets/subnet1"
-    private_ip_address_allocation = "Static"
+    subnet_id                     = azurerm_subnet.sbnet1.id
     private_ip_address            = var.private_ip_address
+    private_ip_address_allocation = "Static"
   }
 }
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+
+  depends_on = [
+    azurerm_key_vault.kvaultmv1
+  ]
+}
+
+resource "azurerm_key_vault" "kvaultmv1" {
+  name                = "kvaultmv1"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = var.address_space
-}
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = var.address_prefixes
-}
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
-resource "azurerm_network_interface_backend_address_pool_association" "nic1" {
-  network_interface_id    = azurerm_network_interface.nic1.id
-  ip_configuration_name   = azurerm_network_interface.nic1.ip_configuration[0].name
-  backend_address_pool_id = azurerm_lb_backend_address_pool.pool1.id
-}
-
-resource "azurerm_lb_backend_address_pool" "pool1" {
-  name                = "pool1"
-  loadbalancer_id     = azurerm_lb.lb1.id
-}
-
-resource "azurerm_lb" "lb1" {
-  name                = "lb1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  sku                 = "Basic"
-
-  frontend_ip_configuration {
-    name                          = "PublicIPAddress"
-    public_ip_address_id          = azurerm_public_ip.publicip1.id
-    private_ip_address_allocation = "Dynamic"
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge"
+    ]
   }
 }
 
-resource "azurerm_public_ip" "publicip1" {
-  name                = "publicip1"
+data "azurerm_key_vault_secret" "publicclave" {
+  name         = "publicclave"
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
+}
+
+data "azurerm_key_vault_secret" "secretclave" {
+  name         = "secretclave"
+  key_vault_id = azurerm_key_vault.kvaultmv1.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  size                = "Standard_B2s"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_username = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = data.azurerm_key_vault_secret.publicclave.value
+  }
 }
 
 variable "address_space" {}
