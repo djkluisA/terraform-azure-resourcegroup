3935b67620e5a89fb0532c9dec949009b144462c
djkluisA <djkluis@gmail.com>
2023-05-26 15:02:30 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -7,8 +7,9 @@ variable "private_ip_address" {}
 # Configuración del proveedor de AWS
 provider "aws" {
   region = "us-east-1"
-  skip_provider_registration = true
-  features {}
+  skip_metadata_api_check = true
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
 }
 
 # Creación de la red virtual
