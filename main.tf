d9954fde878fead2acc7e99e8484b12e2df82a4c
djkluisA <djkluis@gmail.com>
2023-05-31 13:14:35 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -48,9 +48,9 @@ resource "tls_private_key" "key" {
 
   lifecycle {
     ignore_changes = [
-      "private_key_pem",
-      "public_key_openssh",
-      "public_key_pem"
+      tls_private_key.key.private_key_pem,
+      tls_private_key.key.public_key_openssh,
+      tls_private_key.key.public_key_pem
     ]
   }
 }
@@ -104,6 +104,8 @@ resource "azurerm_linux_virtual_machine" "vm1" {
     username   = "azureuser"
     public_key = azurerm_key_vault_secret.publicclave.value
   }
+
+  network_interface_ids = [azurerm_network_interface.nic1.id]
 }
 
 resource "azurerm_public_ip" "pipbastion" {
