51e4b3f8a46d0c5df5ab6d1f3e2938c225cc0672
djkluisA <djkluis@gmail.com>
2023-05-31 13:57:36 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,4 +1,57 @@
 
-network_interface_ids = [
-    azurerm_network_interface.nic1.id
-]
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "resource_group1-a6e44407-playground-sandbox"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+  }
+}
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = "vnet1"
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_interface_security_group_association" "nic1_nsg_association" {
+  network_interface_id      = azurerm_network_interface.nic1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
+}
+
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+variable "address_space" {}
+
+variable "address_prefixes" {}
+
+variable "address_prefixes2" {}
+
+variable "private_ip_address" {}
