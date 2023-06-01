e2bd5b6a54b528489a875000394e071f06fb08af
djkluisA <djkluis@gmail.com>
2023-06-01 21:09:59 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -128,11 +128,12 @@ resource "azurerm_bastion_host" "vm1host" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   sku                 = "Standard"
-  ip_configuration   = [{
+  
+  ip_configuration {
     name                          = "vm1connect"
     subnet_id                     = azurerm_subnet.AzureBastionSubnet.id
     public_ip_address_id          = azurerm_public_ip.pipbastion.id
-  }]
+  }
 }
 
 variable "address_space" {}
