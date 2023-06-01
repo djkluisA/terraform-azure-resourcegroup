55e34d3e118453993e25c453df57fefc23743d54
djkluisA <djkluis@gmail.com>
2023-06-01 20:11:13 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -11,11 +11,6 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_client_config" "current" {}
 
-variable "address_space" {}
-variable "address_prefixes" {}
-variable "address_prefixes2" {}
-variable "private_ip_address" {}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   address_space       = var.address_space
