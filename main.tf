72986a4b6351cb845a5f7320975381a9810b5890
djkluisA <djkluis@gmail.com>
2023-06-01 20:56:32 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
-Lo siento, parece que hubo un malentendido. La respuesta anterior fue una guía general para comenzar con Terraform y Azure Cloud, no un código específico para tu escenario. Además, el error que recibiste se debió a que el código que proporcionaste no era válido y contenía un bloque no reconocido.
+' para que sea más fácil de leer.
 
-Para proporcionar un código específico para tu escenario, necesitaría más información sobre lo que estás tratando de lograr. ¿Podrías proporcionar más detalles sobre lo que estás intentando hacer y qué recursos de Azure estás utilizando? Con esa información, puedo ayudarte a crear un código de Terraform específico para tu caso.
+Aquí te proporciono un ejemplo de código que cumple con estas recomendaciones:
+
