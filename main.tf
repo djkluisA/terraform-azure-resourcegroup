dec9701878ce7f4385c4eae286c51f34588f12b2
djkluisA <djkluis@gmail.com>
2023-06-01 19:53:12 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -42,14 +42,10 @@ resource "tls_private_key" "key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 
-  depends_on = [
-    azurerm_key_vault.kvaultmv1310520231
-  ]
-
   lifecycle {
     ignore_changes = [
-      tls_private_key.key.public_key_pem,
-      tls_private_key.key.private_key_pem
+      public_key_pem,
+      private_key_pem
     ]
   }
 }
