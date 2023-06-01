0a95e420df049cafe5f70810ff0de5fce53d1d3f
djkluisA <djkluis@gmail.com>
2023-06-01 20:56:59 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,4 +1,46 @@
-' para que sea más fácil de leer.
 
-Aquí te proporciono un ejemplo de código que cumple con estas recomendaciones:
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
 
+data "azurerm_resource_group" "example" {
+  name = "resource group1-a6e44407-playground-sandbox"
+}
+
+data "azurerm_client_config" "current" {}
+
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "address_prefixes2" {}
+variable "private_ip_address" {}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  subnet {
+    name           = "example-subnet"
+    address_prefix = var.address_prefixes
+  }
+
+  subnet {
+    name           = "example-subnet2"
+    address_prefix = var.address_prefixes2
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_virtual_network.example.subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+  }
+}
