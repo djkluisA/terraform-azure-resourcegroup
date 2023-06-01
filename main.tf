cf318d07b8d38753f236f3f2057f9ce2359955f1
djkluisA <djkluis@gmail.com>
2023-06-01 19:33:49 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_key_vault_access_policy" "kvap" {
   object_id = data.azurerm_client_config.current.object_id
 
   secret_permissions = [
-    "get",
+    "Get",
   ]
 }
 
@@ -90,3 +90,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
     public_key = tls_private_key.ssh.public_key_openssh
   }
 }
+
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "address_prefixes2" {}
+variable "private_ip_address" {}
