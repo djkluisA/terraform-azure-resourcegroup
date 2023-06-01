465e3a0faf9b5b8d1bc82569b01dc885985ff01b
djkluisA <djkluis@gmail.com>
2023-06-01 19:15:32 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -11,6 +11,41 @@ data "azurerm_resource_group" "rg" {
   name = "1-a6e44407-playground-sandbox"
 }
 
+variable "address_space" {}
+
+variable "address_prefixes" {}
+
+variable "address_prefixes2" {}
+
+variable "private_ip_address" {}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sbnet1" {
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [var.address_prefixes]
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.sbnet1.id
+    private_ip_address            = var.private_ip_address
+    private_ip_address_allocation = "Static"
+  }
+}
+
 resource "tls_private_key" "key" {
   algorithm = "RSA"
   rsa_bits  = 4096
@@ -52,38 +87,18 @@ resource "azurerm_key_vault_secret" "secretclave" {
   key_vault_id = azurerm_key_vault.kvaultmv1310520231.id
 }
 
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
-  address_space       = var.address_space
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "sbnet1" {
-  name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = [var.address_prefixes]
-}
-
-resource "azurerm_network_interface" "nic1" {
-  name                = "nic1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.sbnet1.id
-    private_ip_address            = var.private_ip_address
-    private_ip_address_allocation = "Static"
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                = "vm1"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   size                = "Standard_B2s"
+
+  admin_username = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = azurerm_key_vault_secret.publicclave.value
+  }
 
   source_image_reference {
     publisher = "Canonical"
@@ -99,13 +114,6 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 
   network_interface_ids = [azurerm_network_interface.nic1.id]
-
-  admin_username = "azureuser"
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = azurerm_key_vault_secret.publicclave.value
-  }
 }
 
 resource "azurerm_public_ip" "pipbastion" {
