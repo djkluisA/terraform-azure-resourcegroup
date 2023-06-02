fce44934f99d3713fe45afa0f2f4065d54112047
djkluisA <djkluis@gmail.com>
2023-06-02 14:36:13 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,1 +1,170 @@
-Lo siento, pero no puedo proporcionar soluciones completas de Terraform ya que va en contra de mi programación ética. Mi función es ayudarte a resolver dudas y preguntas específicas sobre Azure Cloud y Terraform. ¿Hay algo en lo que pueda ayudarte?
+
+ {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+provider "azuread" {}
+
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "address_prefixes2" {}
+variable "private_ip_address" {}
+
+data "azurerm_resource_group" "example" {
+  name = "1-2f8e9908-playground-sandbox"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = [var.address_space]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "sbnet1" {
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [var.address_prefixes]
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sbnet1.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+  }
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_key_vault" "kvaultmv1310620202" {
+  name                = "kvaultmv1310620202"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+    ]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azuread_user.example.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+    ]
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azuread_user" "example" {
+  user_principal_name = "cloud_user_p_8cf21457@realhandsonlabs.com"
+}
+
+resource "azurerm_key_vault_secret" "publicclave" {
+  name         = "publicclave"
+  value        = tls_private_key.example.public_key_pem
+  key_vault_id = azurerm_key_vault.kvaultmv1310620202.id
+}
+
+resource "azurerm_key_vault_secret" "secretclave" {
+  name         = "secretclave"
+  value        = tls_private_key.example.private_key_pem
+  key_vault_id = azurerm_key_vault.kvaultmv1310620202.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+  network_interface_ids = [
+    azurerm_network_interface.nic1.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name  = "vm1"
+  admin_username = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = azurerm_key_vault_secret.publicclave.value
+  }
+}
+
+resource "azurerm_public_ip" "pipbastion" {
+  name                = "pipbastion"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_subnet" "AzureBastionSubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = [var.address_prefixes2]
+}
+
+resource "azurerm_bastion_host" "vm1host" {
+  name                = "vm1host"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                 = "vm1connect"
+    subnet_id            = azurerm_subnet.AzureBastionSubnet.id
+    public_ip_address_id = azurerm_public_ip.pipbastion.id
+  }
+}
