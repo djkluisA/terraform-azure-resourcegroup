6cd062e3d7f1e89dc0d3ebc93cd7d5179f3a0b54
djkluisA <djkluis@gmail.com>
2023-06-02 14:15:46 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -10,6 +10,22 @@ data "azurerm_resource_group" "example" {
 
 data "azurerm_client_config" "current" {}
 
+data "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  virtual_network_name = data.azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "address_prefixes2" {}
+variable "private_ip_address" {}
+
 output "resource_group_id" {
   value = data.azurerm_resource_group.example.id
 }
