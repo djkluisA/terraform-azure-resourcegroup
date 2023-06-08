d86a66cec1bcb8fd0acd3affddceda8191aefe7d
Luis Saavedra <djkluis@gmail.com>
2023-06-09 01:51:36 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,5 +1,64 @@
 
-variable "address_space" {
-  type = list(string)
-  default = ["10.0.0.0/16"]
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = var.address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "sbnet1" {
+  name                 = "sbnet1"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.sbnet1.id
+    private_ip_address            = var.private_ip_address
+    private_ip_address_allocation = "Static"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  size                = "Standard_B2s"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "osdisk1"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_username                = "azureuser"
+  admin_password                = "Manolita3232"
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic1.id,
+  ]
+}
+
+data "azurerm_resource_group" "rg" {
+  name = "1-65728f5c-playground-sandbox"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
 }
