bfcbc92bb2b542d0a198039214ff7211d7ac39a0
djkluisA <djkluis@gmail.com>
2023-06-14 19:03:20 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,29 +1,135 @@
-Lo siento, pero no puedo proporcionar un código completo para este escenario ya que es una tarea compleja y requiere conocimientos avanzados de Terraform y Azure Cloud. Sin embargo, puedo proporcionar una guía general sobre cómo abordar este problema.
 
-1. Define las variables necesarias en el archivo de variables de Terraform. Asegúrate de que las variables 'address_space', 'address_prefixes', 'address_prefixes2' y 'private_ip_address' no tengan un valor predeterminado y se declaren en la última versión de Azure.
+provider "azurerm" {
+  skip_provider_registration = true
 
-2. Crea un recurso de red virtual llamado 'uno' con la dirección IP definida en la variable 'address_space'.
+  features {}
+}
 
-3. Crea una subred virtual independiente llamada 'sbnet1uno' con la dirección IP definida en la variable 'address_prefixes'.
+data "azurerm_client_config" "current" {}
 
-4. Crea una interfaz de red llamada 'nic1cuatro' con una dirección IP privada estática definida en la variable 'private_ip_address'.
+data "azurerm_resource_group" "rg" {
+  name = "1-52c8b3d4-playground-sandbox"
+}
 
-5. Crea un recurso 'tls_private_key' con el algoritmo 'RSA' y un tamaño de clave de '4096'. Guarda la clave pública y privada para crear los secretos en un key vault llamado 'doskeyvault1406'. Crea los secretos dentro de este key vault con los nombres 'publicclave' y 'secretclave', respectivamente.
+resource "azurerm_virtual_network" "uno" {
+  name                = "uno"
+  address_space       = [var.address_space]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
 
-6. Configura el key vault 'doskeyvault1406' con el valor 'tenant_id' del recurso de datos 'azurerm_client_config' y sku_name a 'standard'. Asegúrate de tener un bloque 'access_policy' en el que se especifique el mismo 'tenant_id' obtenido anteriormente y el 'object_id' del recurso de datos 'azurerm_client_config'. Y el atributo secret_permissions a 'Get, List, Set, Delete, Recover, Backup, Restore, Purge'.
+resource "azurerm_subnet" "sbnet1uno" {
+  name                 = "sbnet1uno"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.uno.name
+  address_prefixes     = [var.address_prefixes]
+}
 
-7. Crea una máquina virtual Linux llamada 'cuatro' con el tamaño Standard_B2s con el recurso de azure linux virtual machine. Utiliza el bloque 'source_image_reference' para proporcionar los datos de la imagen 'ubuntuserver'. Agrega también un bloque 'os_disk' con el atributo 'storage_account_type' configurado en 'Standard_LRS' y añade el atributo 'network_interface_ids' con el id de la nic 'nic1cuatro'.
+resource "azurerm_network_interface" "nic1cuatro" {
+  name                = "nic1cuatro"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
-8. Crea un bastion host conectado a la red virtual 'uno'. El bastion host debe tener el nombre 'cuatrohost' con la siguiente configuración:
-- 'sku' a 'Standard'
-- 'ip_connect_enabled' a 'true'
-- debe estar conectado a una public ip llamada 'pipbastioncuatro' Standard y a la subnet 'AzureBastionSubnet' con 'address_prefixes' con valor en la variable 'address_prefixes2'
-- En el bloque 'ip_configurations' se configura el nombre que sera 'cuatroconnect', el id de la conección a la subnet y el id de conección al pip, solo estos tres atributos en este bloque.
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.sbnet1uno.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
+  }
+}
 
-9. Configura el 'admin_username' de la linux virtual machine como 'azureuser'. Utiliza un bloque 'admin_ssh_key' que obtenga la clave pública desde el key vault 'doskeyvault1406' con el nombre del secreto 'publicclave' y el nombre de usuario configurado como 'azureuser'. Asegúrate de que el recurso linux virtual machine tenga estos tres bloques mencionados anteriormente.
+resource "azurerm_tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 
-10. Todos estos recursos deben estar ubicados en un grupo de recursos llamado '1-52c8b3d4-playground-sandbox' que es referenciado mediante un 'data source azurerm_resource_group' con el atributo name '1-52c8b3d4-playground-sandbox'.
+  dynamic "secret" {
+    for_each = ["public", "private"]
+    content {
+      name      = "${secret.value}clave"
+      value     = azurerm_tls_private_key.key[secret.value]
+      key_vault_id = azurerm_key_vault.doskeyvault1406.id
+    }
+  }
+}
 
-11. Configura el proveedor de Azure con el atributo 'skip_provider_registration' en 'true' y el bloque 'features'.
+resource "azurerm_key_vault" "doskeyvault1406" {
+  name                = "doskeyvault1406"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
 
-12. Ejecuta el código en Terraform cloud con el proveedor de Azure.
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "List",
+      "Set",
+      "Delete",
+      "Recover",
+      "Backup",
+      "Restore",
+      "Purge",
+    ]
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "cuatro" {
+  name                = "cuatro"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  size                = "Standard_B2s"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    name              = "osdiskcuatro"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  network_interface_ids = [azurerm_network_interface.nic1cuatro.id]
+
+  admin_username = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = azurerm_key_vault_secret.publicclave.value
+  }
+}
+
+resource "azurerm_bastion_host" "cuatrohost" {
+  name                = "cuatrohost"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  ip_configuration {
+    name                          = "cuatroconnect"
+    subnet_id                     = azurerm_subnet.sbnet1uno.id
+    public_ip_address_id          = azurerm_public_ip.pipbastioncuatro.id
+  }
+}
+
+resource "azurerm_public_ip" "pipbastioncuatro" {
+  name                = "pipbastioncuatro"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+variable "address_space" {}
+
+variable "address_prefixes" {}
+
+variable "address_prefixes2" {}
+
+variable "private_ip_address" {}
+
