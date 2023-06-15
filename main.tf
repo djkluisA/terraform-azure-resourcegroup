66d022a1baa3ffcdf4020ca358ab4d9533514c6e
djkluisA <djkluis@gmail.com>
2023-06-15 15:46:44 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-
+hcl
 provider "azurerm" {
   skip_provider_registration = true
   features {}
@@ -8,29 +8,11 @@ data "azurerm_resource_group" "example" {
   name = "1-2732064a-playground-sandbox"
 }
 
-data "azurerm_client_config" "current" {}
-
-variable "address_space" {
-  type = list(string)
-}
-
-variable "address_prefixes" {
-  type = list(string)
-}
-
-variable "address_prefixes2" {
-  type = list(string)
-}
-
-variable "private_ip_address" {
-  type = string
-}
-
 resource "azurerm_virtual_network" "uno" {
   name                = "uno"
+  address_space       = var.address_space
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-  address_space       = var.address_space
 }
 
 resource "azurerm_subnet" "sbnet1uno" {
@@ -84,29 +66,29 @@ resource "azurerm_key_vault" "doskeyvault1406" {
 
 resource "azurerm_key_vault_secret" "publicclave" {
   name         = "publicclave"
-  key_vault_id = azurerm_key_vault.doskeyvault1406.id
   value        = tls_private_key.example.public_key_pem
+  key_vault_id = azurerm_key_vault.doskeyvault1406.id
 }
 
 resource "azurerm_key_vault_secret" "secretclave" {
   name         = "secretclave"
-  key_vault_id = azurerm_key_vault.doskeyvault1406.id
   value        = tls_private_key.example.private_key_pem
+  key_vault_id = azurerm_key_vault.doskeyvault1406.id
 }
 
 resource "azurerm_linux_virtual_machine" "cuatro" {
   name                = "cuatro"
-  location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
   size                = "Standard_B2s"
-
+  admin_username      = "azureuser"
   network_interface_ids = [
     azurerm_network_interface.nic1cuatro.id,
   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = azurerm_key_vault_secret.publicclave.value
   }
 
   source_image_reference {
@@ -116,39 +98,16 @@ resource "azurerm_linux_virtual_machine" "cuatro" {
     version   = "latest"
   }
 
-  admin_username = "azureuser"
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = azurerm_key_vault_secret.publicclave.value
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 }
 
-resource "azurerm_public_ip" "pipbastioncuatro" {
-  name                = "pipbastioncuatro"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
+variable "address_space" {}
 
-resource "azurerm_subnet" "AzureBastionSubnet" {
-  name                 = "AzureBastionSubnet"
-  resource_group_name  = data.azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.uno.name
-  address_prefixes     = var.address_prefixes2
-}
+variable "address_prefixes" {}
 
-resource "azurerm_bastion_host" "cuatrohost" {
-  name                = "cuatrohost"
-  location            = data.azurerm_resource_group.example.location
-  resource_group_name = data.azurerm_resource_group.example.name
+variable "address_prefixes2" {}
 
-  ip_configuration {
-    name                 = "cuatroconnect"
-    public_ip_address_id = azurerm_public_ip.pipbastioncuatro.id
-    subnet_id            = azurerm_subnet.AzureBastionSubnet.id
-  }
-
-  sku = "Standard"
-}
+variable "private_ip_address" {}
