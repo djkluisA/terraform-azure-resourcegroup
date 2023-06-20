49d3af14b8b4cebd8fd5a897cdc631cdbcbbf971
djkluisA <djkluis@gmail.com>
2023-06-20 15:01:26 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-
- {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-  }
-}
-
+hcl
 provider "azurerm" {
   skip_provider_registration = true
   features {}
