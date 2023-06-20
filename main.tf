91cd9c02bcc922493046b8a670206dbca2daf398
djkluisA <djkluis@gmail.com>
2023-06-20 14:52:46 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
-
- {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.0"
-    }
-  }
-}
-
+hcl
 provider "azurerm" {
   skip_provider_registration = true
   features {}
@@ -19,21 +10,13 @@ data "azurerm_resource_group" "example" {
 
 data "azurerm_client_config" "current" {}
 
-variable "address_space" {
-  type = string
-}
+variable "address_space" {}
 
-variable "address_prefixes" {
-  type = string
-}
+variable "address_prefixes" {}
 
-variable "address_prefixes2" {
-  type = string
-}
+variable "address_prefixes2" {}
 
-variable "private_ip_address" {
-  type = string
-}
+variable "private_ip_address" {}
 
 resource "azurerm_virtual_network" "uno" {
   name                = "uno"
