0cef177ae5b8012b0eef70c3375185cfae902de8
djkluisA <djkluis@gmail.com>
2023-06-21 10:55:42 +0200

    my commit message

--- main.tf
+++ main.tf
@@ -1,64 +1,63 @@
+hcl
+ {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
 
 provider "azurerm" {
   skip_provider_registration = true
-
   features {}
 }
 
-data "azurerm_resource_group" "rg" {
+variable "address_space" {}
+variable "address_prefixes" {}
+variable "address_prefixes2" {}
+variable "private_ip_address" {}
+
+data "azurerm_resource_group" "example" {
   name = "1-add4c5fe-playground-sandbox"
 }
 
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = var.address_space
+resource "azurerm_virtual_network" "vnetopenai" {
+  name                = "vnetopenai"
+  address_space       = [var.address_space]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "sbnet1" {
-  name                 = "sbnet1"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = var.address_prefixes
+resource "azurerm_subnet" "sbnet1vnetopenai" {
+  name                 = "sbnet1vnetopenai"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnetopenai.name
+  address_prefixes     = [var.address_prefixes]
 }
 
-resource "azurerm_network_interface" "nic1" {
-  name                = "nic1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_network_interface" "nic1vmopenai" {
+  name                = "nic1vmopenai"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.sbnet1.id
-    private_ip_address            = var.private_ip_address
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sbnet1vnetopenai.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = var.private_ip_address
   }
 }
 
-resource "tls_private_key" "key" {
+resource "tls_private_key" "example" {
   algorithm = "RSA"
   rsa_bits  = 4096
-
-  depends_on = [
-    azurerm_key_vault.kvault
-  ]
-
-  lifecycle {
-    ignore_changes = [
-      private_key_pem,
-      public_key_openssh,
-      public_key_pem
-    ]
-  }
 }
 
-resource "azurerm_key_vault" "kvault" {
-  name                = "kvaultmv1310520231"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_key_vault" "keyvaultopenai" {
+  name                = "keyvaultopenai"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
@@ -74,16 +73,36 @@ resource "azurerm_key_vault" "kvault" {
       "Recover",
       "Backup",
       "Restore",
-      "Purge"
+      "Purge",
     ]
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "vm1"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_key_vault_secret" "publicclave" {
+  name         = "publicclave"
+  value        = tls_private_key.example.public_key_pem
+  key_vault_id = azurerm_key_vault.keyvaultopenai.id
+}
+
+resource "azurerm_key_vault_secret" "secretclave" {
+  name         = "secretclave"
+  value        = tls_private_key.example.private_key_pem
+  key_vault_id = azurerm_key_vault.keyvaultopenai.id
+}
+
+resource "azurerm_linux_virtual_machine" "vmopenai" {
+  name                = "vmopenai"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
   size                = "Standard_B2s"
+  network_interface_ids = [
+    azurerm_network_interface.nic1vmopenai.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
   source_image_reference {
     publisher = "Canonical"
@@ -92,16 +111,7 @@ resource "azurerm_linux_virtual_machine" "vm1" {
     version   = "latest"
   }
 
-  os_disk {
-    name              = "osdisk"
-    caching           = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id
-  ]
-
+  computer_name  = "vmopenai"
   admin_username = "azureuser"
 
   admin_ssh_key {
@@ -110,49 +120,29 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 }
 
-resource "azurerm_public_ip" "pipbastion" {
-  name                = "pipbastion"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+resource "azurerm_public_ip" "pipbastionvmopenai" {
+  name                = "pipbastionvmopenai"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
 resource "azurerm_subnet" "AzureBastionSubnet" {
   name                 = "AzureBastionSubnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = var.address_prefixes2
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnetopenai.name
+  address_prefixes     = [var.address_prefixes2]
 }
 
-resource "azurerm_bastion_host" "vm1host" {
-  name                = "vm1host"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  sku                 = "Standard"
+resource "azurerm_bastion_host" "vmopenaihost" {
+  name                = "vmopenaihost"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
   ip_configuration {
-    name                          = "vm1connect"
-    subnet_id                     = azurerm_subnet.AzureBastionSubnet.id
-    public_ip_address_id          = azurerm_public_ip.pipbastion.id
+    name                 = "vmopenaiconnect"
+    subnet_id            = azurerm_subnet.AzureBastionSubnet.id
+    public_ip_address_id = azurerm_public_ip.pipbastionvmopenai.id
   }
 }
-
-resource "azurerm_key_vault_secret" "publicclave" {
-  name         = "publicclave"
-  value        = tls_private_key.key.public_key_openssh
-  key_vault_id = azurerm_key_vault.kvault.id
-}
-
-resource "azurerm_key_vault_secret" "secretclave" {
-  name         = "secretclave"
-  value        = tls_private_key.key.private_key_pem
-  key_vault_id = azurerm_key_vault.kvault.id
-}
-
-variable "address_space" {}
-
-variable "address_prefixes" {}
-
-variable "address_prefixes2" {}
-
-variable "private_ip_address" {}
